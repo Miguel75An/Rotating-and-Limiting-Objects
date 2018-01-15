020e1ad6785aae4bbd4ad332d861604d8e0f4aec
Miguel75An <miguel45dom@gmail.com>
2018-01-15 15:07:28 -0500

    Rotate and Translate

---
+++ Assets/Example2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example2 : MonoBehaviour
+{
+
+    public Transform center;
+    private Vector3 v;
+
+
+    void Start()
+    {
+        // Requires the block to be directly to the right of the center
+        //   with rotation set correctly on start
+        v = (transform.position - center.position);
+    }
+
+    void Update()
+    {
+        Vector3 centerScreenPos = Camera.main.WorldToScreenPoint(center.position);
+        Vector3 dir = Input.mousePosition - centerScreenPos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position = center.position + q * v;
+        transform.rotation = q;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float mspeed = 10f;
+    private Vector3 movement = new Vector3(5f, 0f, 0f);
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        PlayerController();
+
+    }
+
+    void PlayerController()
+    {
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movement * mspeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movement * mspeed * Time.deltaTime);
+        }
+
+        Vector3 pos = transform.position;   //Get pos to be a Vector3 var holding all x,y,z components
+        pos.x = Mathf.Clamp(pos.x, -8, 8);  //Clamp only the x component of pos
+        transform.position = pos;           //Update transform position
+    }
+}
